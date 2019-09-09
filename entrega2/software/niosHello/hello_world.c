f9a48952a77af7ab7985affa57df102c70e431a9
Elisa <elisamalzoni@gmail.com>
2019-09-09 14:41:39 -0700

    C entrega 2

--- entrega2/software/niosHello/hello_world.c
+++ entrega2/software/niosHello/hello_world.c
@@ -9,6 +9,7 @@
 volatile int edge_capture;
 int n = 0;
 int novo_valor = 0;
+//int enable = 0;
 
 int main(void){
 
@@ -17,41 +18,27 @@ int main(void){
   printf("Embarcados++ \n");
   volatile unsigned int *p_led = (unsigned int *)(PIO_0_BASE);
   volatile unsigned int *p_sw = (unsigned int *)(PIO_1_BASE);
+  volatile unsigned int *p_en = (unsigned int *)(PIO_2_BASE);
 
-  /*
-  while(1){
-
-	  *(p_led + 1) = 0x02;
-
-  }
-
-  while(1){
-      if (led <= 5){
-          IOWR_32DIRECT(PIO_0_BASE, 0, 0x01 << led++);
-          usleep(100000 >>  *p_sw);
-      }
-      else{
-          led = 0;
-      }
-  };
- */
 	init_1_pio();
+	init_2_pio();
 	uint freq = 0;
 	while(1){
+		if (!*p_en){
+			if(novo_valor){
+				freq = *p_sw;
+				novo_valor = 0;
+			}
 
-		if(novo_valor){
-			freq = *p_sw;
-			novo_valor = 0;
+				IOWR_32DIRECT(PIO_0_BASE, 0, 0x1f);
+				usleep(800000 >>  freq);
+				IOWR_32DIRECT(PIO_0_BASE, 0, 0);
+				usleep(800000 >>  freq);
+
+
+		}else{
+			IOWR_32DIRECT(PIO_0_BASE, 0, 0x00);
 		}
-
-
-		if (led <= 5){
-		          IOWR_32DIRECT(PIO_0_BASE, 0, 0x01 << led++);
-		          usleep(100000 >>  freq);
-		      }
-		      else{
-		          led = 0;
-		      }
 
 	}
 
@@ -85,6 +72,33 @@ void init_1_pio()
     /* Register the interrupt handler. */
     alt_irq_register( PIO_1_IRQ, edge_capture_ptr,
                       handle_sw_interrupts );
+}
+void handle_en_interrupts(void* context, alt_u32 id)
+{
+    /* Cast context to edge_capture's type. It is important that this be
+     * declared volatile to avoid unwanted compiler optimization.
+     */
+    volatile int* edge_capture_ptr = (volatile int*) context;
+    /* Store the value in the Button's edge capture register in *context. */
+    *edge_capture_ptr = IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_2_BASE);
+    /* Reset the Button's edge capture register. */
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_2_BASE, 0);
+
+    //enable = 1;
+}
+
+void init_2_pio()
+{
+    /* Recast the edge_capture pointer to match the alt_irq_register() function
+     * prototype. */
+    void* edge_capture_ptr = (void*) &edge_capture;
+    /* Enable first four interrupts. */
+    IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_2_BASE, 0xF);
+    /* Reset the edge capture register. */
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_2_BASE, 0x0);
+    /* Register the interrupt handler. */
+    alt_irq_register( PIO_2_IRQ, edge_capture_ptr,
+                      handle_en_interrupts );
 }
 
 
